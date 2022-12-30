64cb0a5479114a89dbe4edfbfa14c2345fc0f221
Nikolay Ostrev <107575015+ostrev@users.noreply.github.com>
2022-12-30 23:39:53 +0200

    Update parking_lot.py

--- tuples_and_sets/parking_lot.py
+++ tuples_and_sets/parking_lot.py
@@ -2,6 +2,7 @@ number_of_cars = int(input())
 parking = set()
 for _ in range(number_of_cars):
     command, plate = input().split(', ')
+    
     if command == 'IN':
         parking.add(plate)
     else:
