e6115c2b12621f45709d6bdbc2a78fec4785c4e7
ostrev <npostrev@gmail.com>
2023-01-02 21:39:03 +0200

    Revert "Merge branch 'main' of https://github.com/ostrev/advanced_softuni"
    This reverts commit ddd3ff873acb94f31c6ed59b06624ac8a71b3eb8, reversing
    changes made to 64cb0a5479114a89dbe4edfbfa14c2345fc0f221.

--- tuples_and_sets/parking_lot.py
+++ tuples_and_sets/parking_lot.py
@@ -1,14 +1,5 @@
-number_of_commands = int(input())
-
+number_of_cars = int(input())
 parking = set()
-<<<<<<< HEAD
-for _ in range(number_of_commands):
-    data = input().split(', ')
-    if data[0] == 'IN':
-        parking.add(data[1])
-    elif data[0] == 'OUT':
-        parking.remove(data[1])
-=======
 for _ in range(number_of_cars):
     command, plate = input().split(', ')
     
@@ -16,13 +7,9 @@ for _ in range(number_of_cars):
         parking.add(plate)
     else:
         parking.discard(plate)
->>>>>>> 64cb0a5479114a89dbe4edfbfa14c2345fc0f221
 
 if parking:
-    print(*parking, sep='\n')
+    for car in parking:
+        print(car)
 else:
-<<<<<<< HEAD
-    print("Parking Lot is Empty")
-=======
     print('Parking Lot is Empty')
->>>>>>> 64cb0a5479114a89dbe4edfbfa14c2345fc0f221
